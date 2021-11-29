4b9d0cefe7add9ec94f81a088d7ac3a0a88c50cc
bsocfgbrkls <94411452+Yunshan-li@users.noreply.github.com>
2021-11-29 14:24:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesjeet" href=qwq.css">
+    <link rel="stylesheet" href=qwq.css">
+    <link rel="javascript" href=script.js">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
